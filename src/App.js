5a1af4bea5633f3d766922419dfa5dd50d390e75
Tanuja <tanujatiwari04@gmail.com>
2022-04-15 14:26:39 +0530

    add dark mode

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { useState } from "react";
 import TodoApp from "./TodoApp";
+import { useTheme, ThemeProvider, createTheme } from '@mui/material/styles';
 
 function App() {
+  const [appTheme, setAppTheme] = useState('light')
+  const theme = createTheme({
+    palette: {
+      mode: appTheme === 'light' ? "light" : "dark",
+    }
+  });
+
   return (
-    <TodoApp />
+    <ThemeProvider theme={theme}>
+      <TodoApp appTheme={appTheme} setAppTheme={setAppTheme} />
+    </ThemeProvider>
   );
 }
 
--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,9 +1,10 @@
-import { Typography, Grid, Toolbar, AppBar, Paper } from '@mui/material'
+import { Typography, Grid, Toolbar, AppBar, Paper, IconButton, ListItemSecondaryAction } from '@mui/material'
+import ToggleOffOutlinedIcon from '@mui/icons-material/ToggleOffOutlined';
+import ToggleOnIcon from '@mui/icons-material/ToggleOn';
 import TodoList from './TodoList'
 import TodoForm from './TodoForm'
 import useTodoState from './hooks/useTodoState'
-
-function TodoApp() {
+function TodoApp({ appTheme, setAppTheme }) {
     const initialTodos = JSON.parse(window.localStorage.getItem("todos") || "[]")
     const { todos, addTodo, deleteTodo, toggleTodo, editTodo } = useTodoState(initialTodos)
     return (
@@ -11,11 +12,18 @@ function TodoApp() {
             padding: 0,
             margin: 0,
             height: "100vh",
-            backgroundColor: "#fafafa"
         }}>
             <AppBar color="primary" position='static' style={{ height: '64px' }}>
                 <Toolbar>
                     <Typography color="inherit">Todos with Hooks</Typography>
+                    <ListItemSecondaryAction>
+                        <IconButton onClick={() => {
+                            if (appTheme === 'light') setAppTheme('dark')
+                            else setAppTheme('light')
+                        }}>
+                            {appTheme === 'light' ? <ToggleOffOutlinedIcon fontSize="large" style={{ color: "#eaeaea" }} /> : <ToggleOnIcon fontSize="large" />}
+                        </IconButton>
+                    </ListItemSecondaryAction>
                 </Toolbar>
             </AppBar>
             <Grid container justifyContent="center" style={{ marginTop: '1rem' }}>
